da39c2acde32d0d13a52dc3fa4963773f3aca103
ebru35 <146671039+ebru35@users.noreply.github.com>
2025-12-23 15:42:15 +0000

    Updated root Controller

---
+++ src/main/java/com/projects/project_tracker/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.projects.project_tracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+@GetMapping("/")
+public String root() {
+    return "forward:/projects";
+}
+
+}
